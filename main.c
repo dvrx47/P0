2036359fffdf2db2f4bd5ce8053814d076ac7fc9
Mikołaj Kowalik <kowalik_mikolaj@wp.pl>
2016-10-28 20:59:48 +0200

    zrobiony program

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <pthread.h>
 
+void* funkcjaWatek(void* dane){
+	printf("%s", (char*)dane );
+	return NULL;
+}
+
 int main(){
+	pthread_t watek1, watek2;
+	printf("napis z watku glownego\n");
+	pthread_create(&watek1, NULL, funkcjaWatek, "napis z watku potomnego nr 1\n");
+	pthread_create(&watek2, NULL, funkcjaWatek, "napis z watku potomnego nr 2\n");
+
+	pthread_join(watek1, NULL);
+	pthread_join(watek2, NULL);
 	return 0;
 }
